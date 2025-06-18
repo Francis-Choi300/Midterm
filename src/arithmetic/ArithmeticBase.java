8f696988665536fc045a422195591de7e30e4374
Yoon-Ho Choi <chyoonho@sheridancollege.ca>
2025-06-18 18:12:49 -0400

    Updated Card Class, Created Generator, Added Main method, Decoupled Classes

--- src/arithmetic/ArithmeticBase.java
+++ src/arithmetic/ArithmeticBase.java
@@ -15,25 +15,37 @@ import java.util.Scanner;
  */
 public class ArithmeticBase 
 {
- public double x,y;
-    double calculate(double x, double y) 
+ public double x,y; 
+ public enum OPERATION {ADD, MULTIPLY,SUBTRACT,DIVIDE}
+ private OPERATION op = OPERATION.ADD;
+
+ public ArithmeticBase()
+   {this.op = OPERATION.ADD;}
+
+ public void setOperation (int input){         
+        if (input == 0){this.op = OPERATION.ADD;}
+        else if (input == 1){this.op = OPERATION.SUBTRACT;}
+        else if (input == 2){this.op = OPERATION.MULTIPLY;}
+        else if (input == 3){this.op = OPERATION.DIVIDE;}
+        else {System.out.println("You did not input a number between 0-3!");}}
+ 
+  public double calculate(double x, double y) 
         {
         Scanner sc =new Scanner(System.in);
-        System.out.println("Enter arithmetic operation to Perform: ");
-        String s= sc.next();
-        switch (s.toUpperCase()) 
-        {
-            case "PLUS":
-                return x + y;
-            case "MINUS":
-                return x - y;
-            case "TIMES":
-                return x * y;
-            case "DIVIDE":
-                return x / y;
-            default:
-                throw new AssertionError("Unknown operations " + this);
-        }
-    }
+        System.out.println("Enter arithmetic operation to Perform: 0 ADD,1 SUBTRACT,2 MULTIPLY,3 DIVIDE ");
+        
+        int input = sc.nextInt();
+        
+        this.setOperation(input);
+        
+        if (this.op == OPERATION.ADD){return (x+y);}
+        else if (this.op == OPERATION.SUBTRACT){return (x-y);}
+        else if (this.op == OPERATION.MULTIPLY){return (x*y);}
+        else {return x/y;}
+}
+        
+        
+        
+    
    
 }
